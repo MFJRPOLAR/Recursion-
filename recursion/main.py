2d669628c0ef3078cd4f2a63bf3e7a3122ce1ec3
Sue Ceklosky <cekloskys@chc.edu>
2023-12-04 10:50:25 -0500

    first commit

---
+++ recursion/main.py
@@ -0,0 +1,19 @@
+from loops import * 
+from recursions import  *
+
+print("Factorial of 6 using a loop is:", loops.factorial(6))
+print("Factorial of 5 using a loop is:", loops.factorial(5))
+print("Factorial of 4 using a loop is:", loops.factorial(1))
+
+
+print("Factorial of 6 using a recursion is:", recursion.factorial(6))
+print("Factorial of 5 using a recursion is:", recursion.factorial(5))
+print("Factorial of 4 using a recursion is:", recursion.factorial(1))
+
+print("2 to the 5th power using a loop is:", loops.power(2,5))
+print("2 to the 4th power using a loop is:", loops.power(2,4))
+print("2 to the 0 power using a loop is:", loops.power(2,0))
+
+print("2 to the 5th power using a recursion is:", recursion.power(2,5))
+print("2 to the 4th power using a recursion is:", recursion.power(2,4))
+print("2 to the 0 power using a recursion is:", recursion.power(2,0))
